a5f282080c5a9fba3626ea2411789f12953ce6f4
shmueluzan <vn212733@gmail.com>
2021-01-15 16:03:13 +0200

    Added Spawn Proc

--- windows_api_basics.py
+++ windows_api_basics.py
@@ -1,5 +1,6 @@
 # Import the required module to handle Windows API Calls
 import ctypes
+from ctypes.wintypes import HANDLE, DWORD, WORD,LPBYTE ,LPSTR, LPWSTR, ULONG
 
 class PrintScreen:
     @staticmethod
@@ -15,6 +16,7 @@ class PrintScreen:
         print ("[WARNING] ^ {}".format(message))        
 
 class WindowsAPI:
+    #types for message box
     class MessageBoxType:
         MS_OK           = 0x00000000
         MB_OKCANCEL     = 0x00000001
@@ -23,11 +25,49 @@ class WindowsAPI:
         IDCANCEL        = 2
         IDABORT         = 3
         IDCONTINUE      =11
-    
+    # types for process
     class ProcessAccess:
         # Access Rights
         PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
 
+        #Type of process
+        CREATE_NEW_CONSOLE  = 0x00000010 # The new process has a new console, instead of inheriting it parent's console
+
+        #dwFlags
+        STARTF_USERSHOWWINDOW = 0x00000001
+    #Structure for process info
+    class PROCESS_INFORMATION(ctypes.Structure):
+        _fields_ = [
+            ("hProcess", HANDLE),
+            ("hThread", HANDLE),
+            ("dwProcessId", DWORD),
+            ("dwThreadId", DWORD)
+        ]
+    #Structure for Startup Info
+    class STARTUPINFRO(ctypes.Structure):
+        _fields_ = [
+            ("cd", DWORD),
+            ("lpReserved", LPWSTR),
+            ("lpDesktop", LPWSTR),
+            ("lpTitle", LPWSTR),
+            ("dwX", DWORD),
+            ("dwY", DWORD),
+            ("dwXSize", DWORD),
+            ("dwYSize", DWORD),
+            ("dwXCountChars", DWORD),
+            ("dwYCountChars", DWORD),
+            ("dwFillAttribute", DWORD),
+            ("dwFlags", DWORD),
+            ("wShowWindow", WORD),
+            ("cbReserved2", WORD),
+            ("lpReserved2", LPBYTE),
+            ("hStdInput", HANDLE),
+            ("hStdOutput", HANDLE),
+            ("hStdError", HANDLE) 
+        ]
+    
+
+    #Block of class
     def __init__(self):
         # Setting Up The Params
 
@@ -69,7 +109,8 @@ class WindowsAPI:
         #hWndParent = 0
         #hWndChildAfter = 0
         lpClassName = None # ctypes.c_char_p(widnowName.encode("utf-8"))
-        lpWidnowName = ctypes.c_char_p(widnowName.encode('utf-8'))
+        #lpWidnowName = ctypes.c_char_p(widnowName.encode('utf-8'))
+        lpWidnowName = LPSTR(widnowName.encode('utf-8'))
         
         # Calling the Windows API Call
         #window_handle = self.__user_handler.FindWindowExA(hWndParent,hWndChildAfter, lpClassName, lpWidnowsName)
@@ -88,7 +129,9 @@ class WindowsAPI:
     def get_windows_thread_process_id(self, handleWidnwos):
         hWnd = handleWidnwos
         # Get the PID of the process at the handle
-        lpdwProcessId = ctypes.c_ulong()
+        #lpdwProcessId = ctypes.c_ulong()
+        lpdwProcessId = DWORD()
+
         # We use byref to pass a pointer to the value as needed by the API Call
         response = self.__user_handler.GetWindowThreadProcessId(hWnd, ctypes.byref(lpdwProcessId))
         # Check to see if the call Completed
@@ -116,7 +159,7 @@ class WindowsAPI:
             self.assert_last_error()
             exit(1)
         else:
-            PrintScreen.log("open_process: Got Handle....")
+            PrintScreen.log("open_process: Got Process Handle....")
         
         return process_handler
 
@@ -132,7 +175,7 @@ class WindowsAPI:
             self.assert_last_error() # check if occur error
             exit(1)
         else:
-            PrintScreen.log("terminate_process: Got Kill...")
+            PrintScreen.log("terminate_process: Killed...")
 
         return response
     
@@ -142,9 +185,49 @@ class WindowsAPI:
         hanldeProcess   = self.open_process(processID)
         response        = self.terminate_process(hanldeProcess, exitCode)
         PrintScreen.log("Status {}".format(response))
+    #Spawn Proc
+    def create_process(self, lpApplicationName, dwCreationFlags, lpProcessInformation, lpStartupInfo):
+        lpApplicationName   = LPSTR(lpApplicationName.encode("utf-8"))  #   LPCSTR
+        lpCommandLine       = None  #   LPSTR
+        lpProcessAttributes = None  #   LPSECUTITY_ATTRIBUTES
+        lpThreadAttributes  = None  #   LPSECUTITY_ATTRIBUTES
+        bInheritHandle      = False  #   Bool
+        #dwCreationFlags     = None  #   DWORD
+        lpEnvironment       = None  #   LPVOID
+        lpCurrentDirectory  = None  #   LPCSTR
+        #lpStartupInfo       = WindowsAPI.STARTUPINFRO()  #   LPSTARTUPINFOA
+        #lpProcessInformation= WindowsAPI.PROCESS_INFORMATION()  #   #LPPROCESS_INFORMATION
+        
+        # Calling the Windows API Call
+        response = self.__kernel_handler.CreateProcessA(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandle, dwCreationFlags, lpEnvironment, lpCurrentDirectory, ctypes.byref(lpStartupInfo), ctypes.byref(lpProcessInformation))
+         # Check to see if we have a valid Handle to the process
+        if response <= 0 or response is None:
+            PrintScreen.err("create_process: Could Not Create Process Handle")
+            self.assert_last_error()
+        else:
+            PrintScreen.log("Process Is Running...")
 
-input_user = "Task Manager"
-input_user = "new 1 - Notepad++"
+        return response
+
+
+# Create WinApi Obj
 api = WindowsAPI()
+
+# Example Message Box 
+#print(api.message_box_a(0, "Message Box Title", "Header Message Box", WindowsAPI.MessageBoxType.MB_OKCANCEL))
+
+# Example Kill Proc
+#input_user = "Task Manager"
+#input_user = "new 1 - Notepad++"
 #api.kill_process(input_user)
-print(api.message_box_a(0, "Message Box Title", "Header Message Box", WindowsAPI.MessageBoxType.MB_OKCANCEL))
+
+# Example Spawn Proc
+#lpApplicationName = "C:\\Windows\\System32\\cmd.exe"
+#dwCreationFlags=WindowsAPI.ProcessAccess.CREATE_NEW_CONSOLE
+#lpProcessInformation= WindowsAPI.PROCESS_INFORMATION()
+#lpStartupInfo       = WindowsAPI.STARTUPINFRO()
+#lpStartupInfo.wShowWindow   = 0x1
+#lpStartupInfo.dwFlags       = WindowsAPI.ProcessAccess.STARTF_USERSHOWWINDOW
+#api.create_process(lpApplicationName, dwCreationFlags, lpProcessInformation, lpStartupInfo)
+
+
